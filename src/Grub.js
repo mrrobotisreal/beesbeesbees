2b9799c15a5793dd6b69b1e24c01af6036182532
Mitchell Wintrow <90mitchwintrow@gmail.com>
2022-02-28 11:23:08 -0700

    Finished Grub superclass and tested to make sure I can commit with an arrow function in my code and ES6 pseudoclassical code.

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,11 @@
 var Grub = function() {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
+};
+
+Grub.prototype.eat = () => {
+  // do something in here
+  console.log('I\'m eating here, so buzz off!');
 };
 
